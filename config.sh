bc16965b5c9eca695a1df41818e06d8c0e40f844
“avalonhse” <avalonhse@gmail.com>
2014-04-28 09:09:37 +0000

    “importing”

--- config.sh
+++ config.sh
@@ -1,3 +1,11 @@
-git init
 git config --global user.name “avalonhse”
 git config --global user.email avalonhse@gmail.com
+
+cd ~/Downloads
+wget http://apt.puppetlabs.com/puppetlabs-release-precise.deb
+sudo dpkg -i puppetlabs-release-precise.deb 
+sudo apt-get -y install puppet 
+sudo chown -R ubuntu:ubuntu /etc/puppet
+
+echo $'#!/bin/sh\nsudo puppet apply /home/ubuntu/EC2/puppet/manifests/site.pp—modulepath=/home/ubuntu/EC2/puppet/modules/  $*'  | sudo tee /usr/local/bin/papply > /dev/null
+sudo chmod a+x /usr/local/bin/papply
