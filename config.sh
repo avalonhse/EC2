e650e5caa7c7091053949dd9b12c0ced854e33fa
“avalonhse” <avalonhse@gmail.com>
2014-04-28 09:52:10 +0000

    “importing”

--- config.sh
+++ config.sh
@@ -7,5 +7,5 @@ sudo dpkg -i puppetlabs-release-precise.deb
 sudo apt-get -y install puppet 
 sudo chown -R ubuntu:ubuntu /etc/puppet
 
-echo $'#!/bin/sh\nsudo puppet apply /home/ubuntu/EC2/puppet/manifests/site.pp—modulepath=/home/ubuntu/EC2/puppet/modules/  $*'  | sudo tee /usr/local/bin/papply > /dev/null
+echo $'#!/bin/sh\nsudo puppet apply /home/ubuntu/EC2/puppet/manifests/site.pp --modulepath=/home/ubuntu/EC2/puppet/modules/  $*'  | sudo tee /usr/local/bin/papply > /dev/null
 sudo chmod a+x /usr/local/bin/papply
