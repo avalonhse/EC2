56f69cc2c3484c0b58e00e7de97209da1f8223b5
“avalonhse” <avalonhse@gmail.com>
2014-04-29 04:46:49 +0000

    add rsa

--- config.sh
+++ config.sh
@@ -10,3 +10,7 @@ sudo chown -R ubuntu:ubuntu /etc/puppet
 
 echo $'#!/bin/sh\nsudo puppet apply /home/ubuntu/EC2/puppet/manifests/site.pp --modulepath=/home/ubuntu/EC2/puppet/modules/  $*'  | sudo tee /usr/local/bin/papply > /dev/null
 sudo chmod a+x /usr/local/bin/papply
+
+cd ~/EC2
+cp id_rsa /home/ubuntu/.ssh/
+sudo chmod 400 /home/ubuntu/.ssh/id_rsa
